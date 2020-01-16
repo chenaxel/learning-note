03bb29eb59023c9b109611faf2f800d8deb91340
chenzhaohui <643084341@qq.com>
2020-01-16 16:01:01 +0800

    alg

---
+++ src/main/java/com/axel/leet/Rotate.java
@@ -0,0 +1,33 @@
+package com.axel.leet;
+
+import java.util.Arrays;
+
+/**
+ * des
+ *
+ * @author chenzhaohui
+ * @date 2020/1/16
+ */
+public class Rotate {
+
+	public static void main(String[] args) {
+		int[][] matrix = new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+		rotate(matrix);
+		System.out.println(Arrays.deepToString(matrix));
+	}
+
+	private static void rotate(int[][] matrix) {
+		int n = matrix.length;
+		int[][] newMatrix = new int[n][n];
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				newMatrix[i][j] = matrix[i][j];
+			}
+		}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				matrix[j][n - 1 - i] = newMatrix[i][j];
+			}
+		}
+	}
+}
