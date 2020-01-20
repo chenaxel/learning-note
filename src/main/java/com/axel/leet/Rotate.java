a27bd0d2860ace7dacd3d49016ca7fc689fa2bf2
chenzhaohui <643084341@qq.com>
2020-01-20 15:33:15 +0800

    alg

--- src/main/java/com/axel/leet/Rotate.java
+++ src/main/java/com/axel/leet/Rotate.java
@@ -20,9 +20,7 @@ public class Rotate {
 		int n = matrix.length;
 		int[][] newMatrix = new int[n][n];
 		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				newMatrix[i][j] = matrix[i][j];
-			}
+			System.arraycopy(matrix[i], 0, newMatrix[i], 0, n);
 		}
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
