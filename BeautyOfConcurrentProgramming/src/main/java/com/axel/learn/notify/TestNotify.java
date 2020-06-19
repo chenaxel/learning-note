a1bca99b3a033af2dd9dd3e7a3c9e3d6d6dcba33
chenzhaohui <643084341@qq.com>
2020-06-19 10:23:15 +0800

    beauty of concurrent programming: notify

---
+++ BeautyOfConcurrentProgramming/src/main/java/com/axel/learn/notify/TestNotify.java
@@ -0,0 +1,62 @@
+package com.axel.learn.notify;
+
+/**
+ * des
+ *
+ * @author chenzhaohui
+ * @date 2020/6/19
+ */
+public class TestNotify {
+	static Object resource = new Object();
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread threadA = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized (resource) {
+					System.out.println("threadA get lock");
+					try {
+						resource.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("threadA end");
+				}
+			}
+		});
+
+		Thread threadB = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized (resource) {
+					System.out.println("threadB get lock");
+					try {
+						resource.wait();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					System.out.println("threadB end");
+				}
+			}
+		});
+
+		Thread threadC = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				synchronized (resource) {
+					System.out.println("threadC notify...");
+					resource.notifyAll();
+				}
+			}
+		});
+
+		threadA.start();
+		threadB.start();
+		threadC.start();
+
+		threadA.join();
+		threadB.join();
+		threadC.join();
+		System.out.println("main over");
+	}
+}
